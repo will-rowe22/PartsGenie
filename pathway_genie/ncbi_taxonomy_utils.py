07e8ab4ba957f8f35f569721ec49c27bb648f33c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-08 04:00:11 +0000

    Adding tarfile member sanitization to extractall()

--- pathway_genie/ncbi_taxonomy_utils.py
+++ pathway_genie/ncbi_taxonomy_utils.py
@@ -43,7 +43,26 @@ def _get_ncbi_taxonomy_files(out_dir):
     '''Downloads and extracts NCBI Taxonomy files.'''
     with tarfile.open(_get_file(out_dir), 'r:gz') as tr:
         temp_dir = tempfile.gettempdir()
-        tr.extractall(temp_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tr, temp_dir)
 
     return os.path.join(temp_dir, 'nodes.dmp')
 
